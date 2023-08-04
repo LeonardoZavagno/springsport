bb54260c3307e918a6ea7b0f1edc90e05a60f050
Leonardo <leonardozavagno@gmail.com>
2023-08-04 22:58:51 +0200

    spring initializr

---
+++ judo/src/main/java/com/springsport/judo/JudoApplication.java
@@ -0,0 +1,13 @@
+package com.springsport.judo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudoApplication.class, args);
+	}
+
+}
---
+++ judo/src/test/java/com/springsport/judo/JudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsport.judo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
