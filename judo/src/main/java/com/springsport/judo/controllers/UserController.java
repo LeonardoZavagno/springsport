f0a6148776910e61d034d66688d7dc4d21cf81b5
Leonardo <leonardozavagno@gmail.com>
2023-08-18 14:26:39 +0200

    add crud on usercontroller

--- judo/src/main/java/com/springsport/judo/controllers/UserController.java
+++ judo/src/main/java/com/springsport/judo/controllers/UserController.java
@@ -2,9 +2,14 @@ package com.springsport.judo.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springsport.judo.models.User;
@@ -20,5 +25,28 @@ public class UserController {
     @GetMapping
     public List<User> list(){
         return userRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public User get(@PathVariable Long id) {
+        return userRepository.getReferenceById(id);
+    }
+
+    @PostMapping
+    public User create(@RequestBody final User user){
+        return userRepository.saveAndFlush(user);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public User update(@PathVariable Long id, @RequestBody User user) {
+        User existingUser = userRepository.getReferenceById(id);
+        BeanUtils.copyProperties(user, existingUser, "user_id");
+        return userRepository.saveAndFlush(existingUser);
     }
 }
--- judo/src/main/java/com/springsport/judo/models/User.java
+++ judo/src/main/java/com/springsport/judo/models/User.java
@@ -1,11 +1,16 @@
 package com.springsport.judo.models;
 
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity(name = "users")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class User {
 
     @Id
@@ -16,7 +21,7 @@ public class User {
 
     private String user_surname;
 
-    public User(){
+    public User() {
     }
 
     public Long getUser_id() {
@@ -42,5 +47,43 @@ public class User {
     public void setUser_surname(String user_surname) {
         this.user_surname = user_surname;
     }
-    
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof User)) {
+            return false;
+        }
+
+        User user = (User) o;
+        return Objects.equals(this.getUser_id(), user.getUser_id())
+                && Objects.equals(this.getUser_name(), user.getUser_name())
+                && Objects.equals(this.getUser_surname(), user.getUser_surname());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getUser_id(), this.getUser_name(), this.getUser_surname());
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sBuilder = new StringBuilder();
+        sBuilder.append("User{");
+        sBuilder.append("id=");
+        sBuilder.append(this.getUser_id());
+        sBuilder.append(", name='");
+        sBuilder.append(this.getUser_name());
+        sBuilder.append('\'');
+        sBuilder.append(", surname='");
+        sBuilder.append(this.getUser_surname());
+        sBuilder.append('\'');
+        sBuilder.append('}');
+
+        return sBuilder.toString();
+    }
+
 }
