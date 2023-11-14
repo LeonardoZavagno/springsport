68479f1ebf598cff261df5cdf4d935119f357b45
Leonardo <leonardozavagno@gmail.com>
2023-11-14 20:21:15 +0100

    web

--- judo/src/main/java/com/springsport/judo/JudoApplication.java
+++ judo/src/main/java/com/springsport/judo/JudoApplication.java
@@ -2,12 +2,17 @@ package com.springsport.judo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class JudoApplication {
+@SpringBootApplication()
+public class JudoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JudoApplication.class, args);
+
+		//maven --> ./mvnw.cmd clean package
+		//index --> http://localhost:8080/judo/index.html
+		//users --> http://localhost:8080/judo/api/v1/users
 	}
 
 }
